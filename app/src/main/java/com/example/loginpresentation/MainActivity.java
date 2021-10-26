4368dfadf3a95783724fbff9fa13d63fe08d83ca
Thamsanqa Keith Miya <keith.miya7@gmail.com>
2021-10-27 00:19:07 +0200

    All C.R.U.D database methods created
    Methods to create, read, update, and delete database records have been written. Buttons to clear all input fields, and view data on Sign Up page have been added.

--- app/src/main/java/com/example/loginpresentation/MainActivity.java
+++ app/src/main/java/com/example/loginpresentation/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.loginpresentation;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
 
     //These are variables for all of the buttons in both User Interfaces. Signup1 is the signup button on the login page and signup2 is the signup button on the signup page
     //Button login; This variable is used in the Log In page
-    Button signup2;
+    //Button signup1; This variable is for used in the Log In page as well
+    Button signup2; //This button is used in the Sign up page to insert a record into the database
+    Button clear; //This button is used in the Sign Up page to clear all string entry fields
+    Button view; //This button is used in the Sign Up page to view all existing database records
     //Button cancel; This variable is used in the Log In page
 
     @Override
@@ -32,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
         email = findViewById(R.id.signupemail);
         password = findViewById(R.id.signuppassword);
         passwordAgain = findViewById(R.id.signuppasswordagain);
+        signup2 = findViewById(R.id.btnInsert);
+        clear = findViewById(R.id.btnClear);
+        view = findViewById(R.id.btnView);
 
         DB = new DBHelper(this); //instantiating the DBHelper to achieve connection with database
 
@@ -49,17 +57,57 @@ public class MainActivity extends AppCompatActivity {
                     String emailTXT = email.getText().toString();
                     String passwordTXT = password.getText().toString();
 
-                    Boolean checkInsertData = DB.insertuserdata(emailTXT, fNameTXT, lNameTXT, passwordTXT); // Boolean variable that will let us know whether the data was successfully entered into the database or not
+                    Boolean checkInsertData = DB.insertUserData(emailTXT, fNameTXT, lNameTXT, passwordTXT); // Boolean variable that will let us know whether the data was successfully entered into the database or not
 
                     //creating a message to notify user when they have successfully signed up or not
                     if (!checkInsertData) {
                         Toast.makeText(MainActivity.this, "Sign Up Unsuccessful", Toast.LENGTH_LONG).show();
                     } else {
                         Toast.makeText(MainActivity.this, "Sign Up Complete", Toast.LENGTH_LONG).show();
+                        firstname.getText().clear();
+                        lastname.getText().clear();
+                        email.getText().clear();
+                        password.getText().clear();
+                        passwordAgain.getText().clear();
                     }
-                }
-                else
+                } else
                     Toast.makeText(MainActivity.this, "Password entries do not match. Please ensure you have entered the correct password in both password fields.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //To clear all input fields of the Sign Up page
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstname.getText().clear();
+                lastname.getText().clear();
+                email.getText().clear();
+                password.getText().clear();
+                passwordAgain.getText().clear();
+            }
+        });
+
+        //To view all database records
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = DB.getData(); //This assigns the res variable to the value of the record that is of type cursor
+                if(res.getCount()==0){
+                    Toast.makeText(MainActivity.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while (res.moveToNext()){
+                    buffer.append("email:"+res.getString(0)+"\n");
+                    buffer.append("firstname:"+res.getString(1)+"\n");
+                    buffer.append("lastname:"+res.getString(2)+"\n");
+                    buffer.append("password:"+res.getString(3)+"\n\n");
+                }
+                AlertDialog.Builder  builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("User Entries");
+                builder.setMessage(buffer.toString());
+                builder.show();
             }
         });
     }
